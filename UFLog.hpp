01fccd622a8cfd90f7f9dfec33b20dde23638c08
TurtleMan20 <122575073+TurtleMan20@users.noreply.github.com>
2023-01-12 22:40:58 +0000

    Add files via upload

---
+++ UFLog.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+enum LogType{
+ Error = 1,
+ Complete = 2,
+ Warning = 3
+};
+
+namespace UFLog {
+
+void Log(std::string message, LogType Type){
+
+   if(Type > 3){
+     std::cout << "[Error] " << "Log Type " << Type << " Does Not Exist Try Again Using LogType::Error, LogType::Complete or LogType::Warning" << "\n";
+   }
+   if(0 > Type){
+     std::cout << "[Error] " << "Log Type " << Type << " Does Not Exist Try Again Using LogType::Error, LogType::Complete or LogType::Warning" << "\n";
+    }
+  
+   if(Type == 1){
+     std::cout << "[Error]   " << message << "\n";
+   }
+  
+   if(Type == 2){
+      std::cout << "[Sucess]  " << message << "\n";
+   }
+  
+   if(Type == 3){
+     std::cout << "[Warning] " << message << "\n";
+   }
+  
+ }
+
+}
